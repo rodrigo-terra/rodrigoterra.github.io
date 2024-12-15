be909bd2b0310e20840ad3ada970904adf0719e0
Rodrigo Terra <rodrigo93terra@gmail.com>
2024-12-14 23:17:35 -0300

    Desenvolvimento da Seção Sobre

--- assets/JS/scripts.js
+++ assets/JS/scripts.js
@@ -7,4 +7,13 @@ function changeTheme(){
   toggleTheme.classList.toggle("bi-sun")
   toggleTheme.classList.toggle("bi-moon-stars")
 }
+
 toggleTheme.addEventListener("click", changeTheme);
+const accordionHeaders = document.querySelectorAll(".accordion__header");
+accordionHeaders.forEach(header => {
+  header.addEventListener("click", () => {
+    const accordionItem = header.parentElement;
+    const accordionActive = accordionItem.classList.contains("active");
+    accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+  })
+})
